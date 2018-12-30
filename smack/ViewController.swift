6e2377d5b01438435df5bb013a7385be0f47cca4
Christopher Gibson <chris.edc.2010@gmail.com>
2018-12-29 22:35:32 -0800

    Initial Commit

---
+++ smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack
+//
+//  Created by Christopher on 12/29/18.
+//  Copyright © 2018 Christopher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
